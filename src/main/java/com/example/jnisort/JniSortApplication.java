2c3c9cf7384156694aac1a609fb01ce1480c4cd5
phrismin <rudenw01@gmail.com>
2021-06-29 20:23:33 +0300

    add Swagger v3, delete TestRun

--- src/main/java/com/example/jnisort/JniSortApplication.java
+++ src/main/java/com/example/jnisort/JniSortApplication.java
@@ -1,11 +1,12 @@
 package com.example.jnisort;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "Title API", version = "2.0", description = "Sorting of array"))
 public class JniSortApplication {
 
     public static void main(String[] args) {
